71a4cd3c1c9ce0330a201103158bb323e7063d49
jeanrjc <jean.cury+kraken@gmail.com>
2020-04-29 17:39:13 +0200

    add scripts

---
+++ scripts/bacterial_slimulation.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import os
+from subprocess import call
+import numpy as np
+import sys
+import pandas as pd
+#import matplotlib.pyplot as plt
+import argparse
+import logging
+import time
+#sys.path.append("/home/jean/Documents/ML_genetics/dnadna/")
+import summary_statistics as ss
+from shutil import copyfile
+import distutils.spawn
+import recapitate as recap
+from multiprocessing import cpu_count, Pool
+
+"""
+Script to launch a given slim3 script with WF or nonWF model.
+In case of WF, it performs first a burnin with ms, loads the msfile, and start the simulation from there.
+In case of nonWF, it performs forward simulation without neutral mutations but keeps track of the tree sequence.
+The tree sequence is outputed and loaded in python with pyslim.
+Pyslim allows then to recapitate the tree, and msprime will add finally the neutral mutation on the recapitated tree.
+"""
+
+def slim(script, run_id, out_dir="."):
+    """Function to call slim
+
+    Parameters
+    ----------
+    script : str
+        Path to a slim script
+
+    Returns
+    -------
+    None
+
+    """
+    slim_bin = distutils.spawn.find_executable("slim3")
+    if slim_bin == None:
+        raise OSError("Slim executable not found")
+    try:
+        with open(os.path.join(out_dir, run_id + ".out"), "w") as outf:
+            Slim_cmd = [slim_bin,
+                        "-t", # print SLiM's total execution time (in user clock time)
+                        "-m", # print SLiM's peak memory usage
+                        "-d", """"runId='{}'" """.format(os.path.join(out_dir, run_id)),
+                        "-d", """"runIdShort='{}'" """.format(run_id),
+                        "-d", """"Ne={}" """.format(Ne),
+                        "-d", """"Mu={}" """.format(mutation_rate),
+                        "-d", """"Rho={}" """.format(recombination_rate),
+                        "-d", """"tractlen={}" """.format(int(tractlen)),
+                        "-d", """"genomeSize={}" """.format(int(chr_size)),
+                        "-d", """"sampleSize={}" """.format(int(sample_size)),
+                        "-d", """"N_generations={}" """.format(int(N_generations)),
+                        script]
+            Slim_cmd2 = " ".join(Slim_cmd)
+            logging.debug("Calling Slim:\n{}".format(Slim_cmd2))
+            ret = call(Slim_cmd2, stdout=outf, stderr=outf, shell=True)
+            if ret != 0:
+                logging.error("Slim command: {}\nfailed with error {}".format(Slim_cmd2, ret))
+                sys.exit(ret)
+    except Exception as e:
+        logging.error("Slim failed: {}".format(e))
+        sys.exit(1)
+
+
+def run_nonWF(run_id, out_dir_nWF, file_suff=""):
+    t1 = time.time()
+    logging.debug("in run nonWF")
+
+    os.makedirs(out_dir_nWF, exist_ok=True)
+
+    if not os.path.isfile(os.path.join(out_dir_nWF, run_id + ".npz")):
+
+        slim(f"nonWF_{params['script']}{file_suff}.slim", run_id, out_dir=out_dir_nWF)
+        time_nWF_fw = time.time() - t1
+        t1 = time.time()
+ 
+        tree = os.path.join(out_dir_nWF, run_id + ".tree")
+        snp_mat, pos = recap.recapitate(tree, sample_size, recombination_rate, mutation_rate, Ne)
+
+        np.savez_compressed(os.path.join(out_dir_nWF, run_id + ".npz"),
+                            SNP=snp_mat,
+                            POS=pos)
+        time_nWF_recap = time.time() - t1
+
+        with open(os.path.join(out_dir_nWF, run_id + ".time"), mode='w') as timefile:
+            timefile.write(f"{run_id}\tnonWF\tfwd\t{time_nWF_fw}\n")
+            timefile.write(f"{run_id}\tnonWF\tburnin\t{time_nWF_recap}\n")
+    return out_dir_nWF
+
+def run_WF(run_id, out_dir_WF, file_suff=""):
+
+    os.makedirs(out_dir_WF, exist_ok=True)
+    logging.debug("in run WF")
+    if not os.path.isfile(os.path.join(out_dir_WF, run_id+".npz")):
+        slim(f"WF_{params['script']}{file_suff}.slim", run_id, out_dir=out_dir_WF)
+        ss.convert_ms(os.path.join(out_dir_WF, run_id+".msout"))
+        try:
+            os.remove(os.path.join(out_dir_WF, run_id+".msout"))
+            os.remove(os.path.join(out_dir_WF, run_id+".ms"))
+        except FileNotFoundError:
+            pass
+    else:
+        logging.info(f"{run_id} already done, continue...")
+    return out_dir_WF
+
+def runner(param_type, run_id, out_dir_nWF, out_dir_WF):
+
+    if param_type in ["both", "nonWF"]:
+        logging.info(f"> Starting nonWF simulations for {run_id}")
+        out_dir_nWF = run_nonWF(run_id, out_dir_nWF, file_suff="")
+        logging.info(f"< nonWF {run_id} simulations done")
+
+    if param_type in ["both", "WF"]:
+        logging.info(f"> Starting WF simulations for {run_id}")
+        out_dir_WF = run_WF(run_id, out_dir_WF, file_suff="")
+        logging.info(f"< WF {run_id} simulations done")
+
+def worker(param):
+    runner(**param)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config",
+                        help="Path to the config file (json)")
+
+    parser.add_argument('--loglevel',
+                         default='INFO',
+                         action='store',
+                         type=str,
+                         metavar='INFO',
+                         help='Amount of log info: DEBUG, [INFO], WARNING, ERROR, CRITICAL')
+
+    parser.add_argument('-rf', '--rescaling_factor',
+                        help="how to rescale the population parameters",
+                        type=int,
+                        default=None)
+    parser.add_argument('-n', '--nrep',
+                    help="number of replicates",
+                    type=int,
+                    default=None)
+
+    parser.add_argument('--cpu',
+                    help="Number of cpu to run the replicates. By default, all available",
+                    type=int,
+                    default=cpu_count())
+
+
+
+    parser.add_argument('--outdir',
+                        help="""Output directory.
+                        Results will under [outdir]/model/rf/, where
+                        mode and rf (rescaling factor) are defined in the config file.
+                        Simulations will not be rerun if the corresponding output file already exists.""",
+                        type=str,
+                        default=".")
+
+
+    args = parser.parse_args()
+    params = json.loads(open(args.config).read())
+
+    logging.basicConfig(stream=sys.stdout,
+                        #filename=os.path.join(out_dir, 'output.log'),
+                        level=logging.getLevelName(args.loglevel),
+                        format="%(asctime)s; %(levelname)s;  %(message)s",
+                        datefmt="%d/%m/%Y %H:%M:%S")
+
+    logging.debug(f"{args.rescaling_factor}")
+
+    if args.rescaling_factor != None:
+        params["rescaling_factor"] = args.rescaling_factor
+    if args.nrep != None:
+        params["N_replicat"] = args.nrep
+
+
+    logging.debug(f"rescaling parameters by a factor of {params['rescaling_factor']}")
+    recombination_rate = params["rho"] * params["rescaling_factor"]
+    logging.debug(f"rho: {params['rho']} -> {recombination_rate}")
+    mutation_rate = params["mu"] * params["rescaling_factor"]
+    logging.debug(f"mu: {params['mu']} -> {mutation_rate}")
+    Ne = int(params["Ne"] / params["rescaling_factor"])
+    logging.debug(f"Ne: {params['Ne']} -> {Ne}")
+
+    sample_size = int(params["sample_size"])
+    tractlen = params["tract_length"]
+    chr_size = int(params["chr_size"])
+    try:
+        N_generations = int(params["N_generations"] / params["rescaling_factor"])
+    except KeyError:
+        N_generations = 1000
+    if recombination_rate * chr_size > 1:
+        logging.warning("""recombination rate too high given chromosome size.
+                        >>> *Recombination rate* is modified to 1/chr_size
+                        (every indiv will endure gene conversion at each generation)""")
+        recombination_rate = 1 / chr_size
+
+    os.makedirs(params["script"], exist_ok=True)
+    copyfile(args.config, os.path.join(params["script"],
+                                       f"params_{params['script']}-{params['rescaling_factor']}_{params['scenario']}"))
+
+
+
+    ##################
+    # Output Results #
+    ##################
+    format_rep = "0>" + str(np.floor(np.log10(params["N_replicat"]) + 1).astype(int))
+    df_time = pd.DataFrame(columns=["WF", "nonWF"])
+    scen_id = "{model}-{rf}_{scenario}".format(model=params["script"],
+                                                         rf=params["rescaling_factor"],
+                                                         scenario=params["scenario"])
+    out_dir_WF = os.path.join(args.outdir,
+                                "{model}/{type}/{scen_id}".format(model=params["script"],
+                                                                type="WF",
+                                                                scen_id=scen_id))
+    out_dir_nWF = os.path.join(args.outdir,
+                                "{model}/{type}/{scen_id}".format(model=params["script"],
+                                                                  type="nonWF",
+                                                                  scen_id=scen_id))
+
+    all_rep_param = []
+    for rep in range(1, params["N_replicat"]+1):
+
+        run_id = "{scen_id}_{replicat:{format_rep}}".format(scen_id=scen_id,
+                                                             replicat=rep,
+                                                             format_rep=format_rep)
+
+        rep_param = {"param_type":params["type"], "run_id":run_id, 
+                     "out_dir_nWF":out_dir_nWF, "out_dir_WF":out_dir_WF}
+        all_rep_param.append(rep_param)
+
+    logging.info(f"start simulations with {cpu_count()} cpu for {len(all_rep_param)} replicates")
+
+    with Pool(processes=args.cpu) as pool:
+        res = pool.map_async(worker, all_rep_param)
+        pool.close()
+        pool.join()
+    logging.info("done")
+
+    if params["type"] in ["both", "WF"]:
+        ss.do_sum_stats(out_dir_WF, ld_kws={"circular":True}, size_chr=chr_size, label="WF")
+        # ss.do_sum_stats(out_dir_WFgs, ld_kws={"circular":True}, size_chr=chr_size, label="WFGS")
+    if params["type"] in ["both", "nonWF"]:
+        ss.do_sum_stats(out_dir_nWF, ld_kws={"circular":True}, size_chr=chr_size, label="nonWF")
+        # ss.do_sum_stats(out_dir_nWFv, ld_kws={"circular":True}, size_chr=chr_size, label="nonWF_var")
+        # ss.do_sum_stats(out_dir_nWFv2, ld_kws={"circular":True}, size_chr=chr_size, label="nonWF_var2")
+        # ss.do_sum_stats(out_dir_nWFv3, ld_kws={"circular":True}, size_chr=chr_size, label="nonWF_var3")
---
+++ scripts/recapitate.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import os
+from subprocess import call
+import numpy as np
+import sys
+import pandas as pd
+import msprime
+import pyslim
+import logging
+
+#import matplotlib.pyplot as plt
+"""
+Script to recapitate haploid simulation
+"""
+
+
+def sample_treeseq(ts, n):
+    """Sample n individuals from the TreeSequence.
+
+    Parameters
+    ----------
+    ts : SlimTreeSequence
+        Full TreeSequence from fwd simulation with tree recording
+    n : int
+        Size of the sample
+
+    Returns
+    -------
+    SlimTreeSequence
+        The subsample tree sequence
+    """
+
+    samples = np.random.choice(ts.samples(),
+                               size=n,
+                               replace=False) # choose n random leaves
+    ts_samp = ts.simplify(samples=samples.astype("int32"))
+    ts_samp_sts = pyslim.SlimTreeSequence(ts_samp)
+    return ts_samp_sts
+
+def recapitate(treesfile, sample_size, recombination_rate, mutation_rate, Ne):
+
+        # load trees with pyslim
+        ts = pyslim.load(treesfile)
+        num_individuals_0 = len(ts.individuals_alive_at(0))
+        n_roots = pd.Series([t.num_roots for t in ts.trees()]).value_counts().to_frame(name="num_tree_with_num_roots")
+        logging.debug(f"""The tree sequence has {ts.num_trees} trees on a genome of length {ts.sequence_length}
+                         {num_individuals_0} alive individuals, {ts.num_samples} 'sample' genomes
+                         and {ts.num_mutations} mutations.
+                         number of roots per tree: 
+                         {n_roots.__str__()[:-12]}""")
+
+        # discard second genomes (diploids) from the tree.
+        #ts_haploid = ts.simplify(samples=[ind.nodes[0] for ind in ts.individuals()])
+        ts_recap = ts.recapitate(recombination_rate=1e-20, 
+                                 Ne=Ne)
+                                 #population_configurations=[msprime.PopulationConiguration(initial_size=Ne)])
+        
+        # simplify to a subset of the haploids
+        sample_inds = np.random.choice(ts_recap.individuals_alive_at(0),
+                                       size=sample_size,
+                                       replace=False) # choose n random leaves                                       
+        sample_nodes = [ts_recap.individual(i).nodes[0] for i in sample_inds]
+        ts_samp = ts_recap.simplify(samples=sample_nodes)
+        
+        n_roots = pd.Series([t.num_roots for t in ts_samp.trees()]).value_counts().to_frame(name="num_tree_with_num_roots")
+        logging.debug(f"""The tree sequence has {ts_samp.num_trees} trees on a genome of length {ts_samp.sequence_length}
+                         {ts_samp.num_individuals} alive individuals, {ts_samp.num_samples} 'sample' genomes
+                         and {ts_samp.num_mutations} mutations.
+                         number of roots per tree: 
+                         {n_roots.__str__()[:-12]}""")
+
+        # mutate
+        ts_mutated = pyslim.SlimTreeSequence(
+                        msprime.mutate(ts_samp,
+                                    rate=mutation_rate/2, # To have 2.Ne.mu and not 4.Ne.mu
+                                    keep=True) # keep existing mutations
+                        )
+        genotype_matrix = ts_mutated.genotype_matrix()
+        snp_mat = genotype_matrix.T
+        pos = np.round(ts_mutated.tables.asdict()["sites"]["position"]).astype(int)
+
+        return snp_mat, pos
+
+# Without actual recapitation
+def fake_recapitate(treesfile, sample_size, recombination_rate, mutation_rate, Ne):
+
+        # load trees with pyslim
+        ts = pyslim.load(treesfile)
+        num_individuals_0 = len(ts.individuals_alive_at(0))
+        logging.debug(f"""The tree sequence has {ts.num_trees} trees on a genome of length {ts.sequence_length}
+                         {num_individuals_0} alive individuals, {ts.num_samples} 'sample' genomes
+                         and {ts.num_mutations} mutations.""")
+
+        # discard second genomes (diploids) from the tree.
+        ts_haploid = ts.simplify(samples=[ind.nodes[0] for ind in ts.individuals()])
+        #ts_recap = ts.recapitate(recombination_rate=1e-20, 
+         #                        Ne=Ne)
+                                 #population_configurations=[msprime.PopulationConiguration(initial_size=Ne)])
+
+        # Keep only alive individuals
+        ts_alive = ts_haploid.simplify(samples=[ind for ind in ts_haploid.individuals_alive_at(0)])
+        logging.debug(f"""The happloid tree sequence has {ts_alive.num_trees} trees on a genome of length {ts_alive.sequence_length}
+                     {ts_alive.num_individuals} alive individuals, {ts_alive.num_samples} 'sample' genomes
+                     and {ts_alive.num_mutations} mutations.""")
+
+        # subsample the number of individuals.
+        ts_sample = sample_treeseq(ts_alive, sample_size)
+
+        ts_mutated = msprime.mutate(ts_sample,
+                                    rate=mutation_rate/2, # To have 2.Ne.mu and not 4.Ne.mu
+                                    keep=True) # keep existing mutations
+
+        genotype_matrix = ts_mutated.genotype_matrix()
+        snp_mat = genotype_matrix.T
+        pos = np.round(ts_mutated.tables.asdict()["sites"]["position"]).astype(int)
+
+        return snp_mat, pos
+
+
+if __name__ == "__main__":
+    pass
